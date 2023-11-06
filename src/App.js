93bee11607e3b452a2e62900de2fad36fc665ff8
geoffreyTeoh <ch.teoh@niuace.com>
2023-11-07 00:55:01 +0800

    amend the css code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="tbc-app-header" style={{ backgroundImage: `url(${tbcphoto})`}}>
         <div className="overlay">
-          <p>Stay Tuned for Our Big Reveal</p>
+          <p>Tuesday Badminton Comunity</p>
+          <p className="tbc-tagline">Stay tuned for Our Major Announcement!</p>
         </div>
       </header>
     </div>
